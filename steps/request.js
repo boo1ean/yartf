9631594ade465b7b406b93c93dc7649c67f0b571
boo1ean <boo1ean0807@gmail.com>
2014-09-15 03:40:55 +0300

    Add ability to use variables in urls

--- steps/request.js
+++ steps/request.js
@@ -1,24 +1,27 @@
 var request = require('request');
+var _ = require('lodash');
+
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
 var Request = function(opt) {
 	this.opt = opt;
 };
 
-var compose_url = function(base, path) {
+var compose_url = function(ctx, path) {
 	if (!path) {
-		return base;
+		return ctx.base_url;
 	}
 
-	return base + path;
+	return ctx.base_url + _.template(path)(ctx.res);
 };
 
 Request.prototype.exec = function(next) {
 	var ctx = this.ctx;
 
 	var options = {
-		url: compose_url(ctx.base_url, this.opt.url),
+		url: compose_url(ctx, this.opt.url),
 		method: this.opt.method,
-		qs: this.opt.method == 'GET' ? this.opt.body : null,
+		qs: this.opt.method == 'GET' ? this.opt.body || null : null,
 		form: this.opt.method == 'POST' ? this.opt.body : null
 	};
 
